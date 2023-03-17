5a86b0ecf805de4ff653802c95b69d9957e70621
Matheus Ellenberger <matheusellenberger60@gmail.com>
2023-03-17 10:11:17 -0300

    update

--- js/listaEventos.js
+++ js/listaEventos.js
@@ -1,48 +1,115 @@
-
-async function main() {
-    const findEvents = await fetch('https://soundgarden-api.vercel.app/events')  // passa a conectionString da API e conecta a ela
-
-    const list = await findEvents.json() //transforma o array (json) que vem da API em um objeto JSON
-
-    const root = document.querySelector('#containerEventos')    //seleciona a TAG já criaada no objeto HTML onde irão cair os dados da API
-
-    //lista e cria os elementos 
-    list.forEach(event => {
-        //criando o article
-        const article = document.createElement('article')
-        //definindo as classes dos articles
-        article.classList.add('evento', 'card', 'p-5', 'm-3')
-
-        //transforma o objeto em data
-        const data = new Date(event.scheduled)
-
-        //cria h2 concatenado com a data formatada acima
-        const h2 = document.createElement('h2')
-        h2.innerText = event.name + ' - ' + data.toString()
-        //insere o elemento criado no article criado acima
-        article.appendChild(h2)
-
-        const h4 = document.createElement('h4')
-        h4.innerText = event.attractions 
-        article.appendChild(h4)
-
-        const p = document.createElement('p')
-        p.innerText = event.description 
-        article.appendChild(p)
-
-        const button = document.createElement('button')
-        button.classList.add('btn', 'btn-primary')
-        button.innerText = 'reservar ingresso'
-        button.addEventListener('click', () => {
-            alert(event.name) // deverá inserir a função
-        })
-        // criar link para a reserva
-        article.appendChild(button)          
-
-        //inser o article dentro do objeto root, que foi o que a gente selecionou acima
-        root.appendChild(article)
-    })
-   
+const URL = 'https://soundgarden-api.vercel.app';
+​
+​
+const evento = document.querySelector("#eventos");
+const modal = document.querySelector("#modal");
+const botaoX = document.querySelector("#btn-x");
+const botaoClose = document.querySelector("#btn-fechar");
+const tituloModal = document.querySelector("#titulo-modal");
+const tickets = document.querySelector("#tickets");
+const form = document.querySelector("#form");
+const email = document.querySelector("#email");
+const ingresso = document.querySelector("#ingresso");
+const nome = document.querySelector("#name");
+const inputId = document.querySelector("#inputId");
+const formularioModal = document.querySelector('#formulario')
+​
+const dataCorreta = (date) => {
+  let data = date.split("");
+  let dataArrumada =
+    data.slice(8, 10).join("") +
+    "/" +
+    data.slice(5, 7).join("") +
+    "/" +
+    data.slice(0, 4).join("");
+  return dataArrumada;
+};
+​
+async function abreModal(id) {
+  // modal.style.display = "flex";
+  formularioModal.style.width = '100vw'
+  formularioModal.style.height = '100vh'
+  formularioModal.style.backgroundColor = 'rgba(0, 0, 0, 0.305)'
+  modal.setAttribute("style", "display:flex");
+  inputId.value = id;
+  const resposta = await fetch(`${URL}/events/${id}`, {
+    method: "GET",
+    redirect: "follow",
+    headers: { "Content-Type": "application/json" },
+  });
+​
+  const conteudoResposta = await resposta.json();
+  tituloModal.innerHTML = `Reserve seu ingresso para ${conteudoResposta.name}`;
+  tickets.innerHTML = `Tickets disponíveis: (${conteudoResposta.number_tickets})`;
+  ingresso.max = conteudoResposta.number_tickets;
 }
-
-main() 
+​
+function fechaModal() {
+    formularioModal.style.width = ''
+  formularioModal.style.height = ''
+  formularioModal.style.backgroundColor = ''
+  modal.setAttribute("style", "display:none");
+  nome.value = "";
+  email.value = "";
+  ingresso.value = "";
+  inputId.value = "";
+}
+​
+async function listarEventos() {
+  const tabela = document.querySelector("tbody");
+  const resposta = await fetch(`${URL}/events`, {
+    method: "GET",
+    redirect: "follow",
+    headers: { "Content-Type": "application/json" },
+  });
+  console.log(resposta);
+​
+  const conteudoResposta = await resposta.json();
+  const bandas = conteudoResposta.slice(0, 12);
+  bandas.forEach((item) => {
+    evento.innerHTML += ` <article class="evento card p-5 m-3">
+    <h2>${item.name} - ${dataCorreta(item.scheduled)}</h2>
+    <h4>${item.attractions}</h4>
+    <p>${item.description}</p>
+    <button class="btn btn-primary" onclick="abreModal('${
+      item._id
+    }')" >reservar ingresso</button>
+    </article>`;
+  });
+}
+listarEventos();
+​
+botaoClose.onclick = () => {
+  fechaModal();
+};
+botaoX.onclick = () => {
+  fechaModal();
+};
+​
+form.onsubmit = async (evento) => {
+  evento.preventDefault();
+​
+  const reservarTicket = {
+    owner_name: nome.value,
+    owner_email: email.value,
+    number_tickets: parseInt(ingresso.value),
+    event_id: inputId.value,
+  };
+​
+  const options = {
+    method: "POST",
+    body: JSON.stringify(reservarTicket),
+    headers: { "Content-Type": "application/json" },
+    redirect: "follow",
+  };
+​
+  const resposta = await fetch(`${URL}/bookings`, options);
+  const conteudoResposta = await resposta.json();
+  console.log(conteudoResposta);
+​
+  if (resposta.status == 201) {
+    alert("Reserva realizada com sucesso");
+​
+    fechaModal();
+  }
+};
